461305b4213c2fe26356485c811e2af624221976
Thomas Biron <thomasbiron.pro@gmail.com>
2018-09-25 15:38:05 +0200

    FourthPush

---
+++ public/javascripts/myScripts.js
@@ -0,0 +1,9 @@
+function createNewArticle(title, picture, text) {
+    var obj = {};
+    obj.title = title;
+    obj.picture = picture;
+    obj.text = text;
+    return obj;
+  }
+  var myArticles = {};
+  myFirstArticle = cre
